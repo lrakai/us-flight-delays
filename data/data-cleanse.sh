381e87b904152a57cc8b7a40d4797daccd2f666e
Logan Rakai <lmrakai@gmail.com>
2018-02-02 13:02:11 -0700

    Include state averages

--- data/data-cleanse.sh
+++ data/data-cleanse.sh
@@ -3,6 +3,7 @@
 raw_data=OnewayT_ONTIME2017.csv
 temp_data=temp-`date +%d-%m-%y-%H-%M-%S`.csv
 cleansed_data=data_by_state.csv
+average_data=state_avg_on_time.csv
 
 header=`head -2 $raw_data`
 
@@ -13,3 +14,12 @@ paste -d ',' $temp_data $raw_data > $cleansed_data
 head -2 $cleansed_data | tail -1 > $temp_data
 tail --lines=+3 $cleansed_data | head --lines=-1 | sort -t ',' -k 1 >> $temp_data
 mv $temp_data $cleansed_data
+
+tail --lines=+3 $cleansed_data | head --lines=-1 | awk -F ',' '{
+    sum[$1]+=$5
+    count[$1]+=1
+   }
+   END {
+     for (key in sum) printf("%s,%s\n", key, sum[key]/count[key])
+   }' \
+   | sort +0n -t ',' -k 2 > $average_data

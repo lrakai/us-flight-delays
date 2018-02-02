1a5a2c7cb863f3a8c277b5b92748ee9a5855f678
Logan Rakai <lmrakai@gmail.com>
2018-02-02 12:55:07 -0700

    Cleanse raw data to organize by state

---
+++ data/data-cleanse.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+raw_data=OnewayT_ONTIME2017.csv
+temp_data=temp-`date +%d-%m-%y-%H-%M-%S`.csv
+cleansed_data=data_by_state.csv
+
+header=`head -2 $raw_data`
+
+echo -en "\nSTATE\n" > $temp_data
+tail --lines=+3 $raw_data | head --lines=-1 | awk -F \: '{print substr($1,length($1)-1,2)}' >> $temp_data
+paste -d ',' $temp_data $raw_data > $cleansed_data
+
+head -2 $cleansed_data | tail -1 > $temp_data
+tail --lines=+3 $cleansed_data | head --lines=-1 | sort -t ',' -k 1 >> $temp_data
+mv $temp_data $cleansed_data
